7c1fe32688eda61d0403d83e8cb76456b25917b2
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2023-01-28 13:29:11 +0100

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Implementation of the insertion sort
+ * @list: head node of the list to be sorted
+ * Return: nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *active, *prev, *buff;
+
+	if (*list == NULL)
+		return;
+
+	prev = *list;
+	active = prev->next;
+	while (active)
+	{
+		buff = active->next;
+		while (prev && prev->n > active->n)
+		{
+			prev->next = active->next;
+			if (active->next)
+				active->next->prev = prev;
+			active->prev = prev->prev;
+			if (prev->prev)
+				prev->prev->next = active;
+			prev->prev = active;
+			active->next = prev;
+
+			prev = active->prev;
+			if (prev == NULL)
+				*list = active;
+			print_list(*list);
+		}
+		active = buff;
+		if (!buff)
+			return;
+		prev = active->prev;
+	}
+}
